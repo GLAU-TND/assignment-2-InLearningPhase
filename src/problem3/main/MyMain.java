f4c9c581c2a06caa5b180a1a3d9e2bd9b27c86a7
Tanesh Gupta <taneshgupta2211@gmail.com>
2020-03-30 11:16:14 +0530

    complete problem 3

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,24 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+import problem3.node.Node;
+import problem5.student.Student;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+
+    public static void main(String[] args) {
+        MyPriorityQueue myPriorityQueue = new MyPriorityQueue();
+        //Adding 5 Students by hard-coding
+        //The Queue enqueues them according to ascending Roll Number as given for example in Problem Statement
+        myPriorityQueue.enqueue(new Node(new Student("A",1)));
+        myPriorityQueue.enqueue(new Node(new Student("B",2)));
+        myPriorityQueue.enqueue(new Node(new Student("C",3)));
+        myPriorityQueue.enqueue(new Node(new Student("D",4)));
+        myPriorityQueue.enqueue(new Node(new Student("E",5)));
+        myPriorityQueue.printQueue();
+    }
 }
